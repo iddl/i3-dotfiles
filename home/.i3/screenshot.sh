676fe9c0f9547551955b15c4085f6ed968f5c562
Ivan DiLernia <ivan@datadoghq.com>
2021-09-24 22:15:14 -0400

    Fix volume changing sinks

--- home/.i3/screenshot.sh
+++ home/.i3/screenshot.sh
@@ -1,5 +1,3 @@
 DESTINATION=~/Dropbox/Screenshots/linux/$(date +"%y%m%d-%H%M%S").png
-killall -9 shutter
-rm -f ~/.shutter/session.xml
 # rm -rf is there to remove empty files if you press escape
 flameshot gui --raw > $DESTINATION || rm -f $DESTINATION
