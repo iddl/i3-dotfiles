dd7a51c3672167a35191a3a0fbd76aa1d7fb2082
Ivan DiLernia <ivan@datadoghq.com>
2024-10-27 16:14:01 -0400

    Use env for cloud storage location

--- home/.i3/screenshot.sh
+++ home/.i3/screenshot.sh
@@ -1,3 +1,3 @@
-DESTINATION=~/Dropbox/Screenshots/linux/$(date +"%y%m%d-%H%M%S").png
+DESTINATION=$DROPBOX/Screenshots/linux/$(date +"%y%m%d-%H%M%S").png
 # rm -rf is there to remove empty files if you press escape
 flameshot gui --raw > $DESTINATION || rm -f $DESTINATION
