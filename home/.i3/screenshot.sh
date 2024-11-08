074d31f9469393eeb8486bcfead21a22069999b0
Ivan DiLernia <ivan@datadoghq.com>
2024-11-08 13:14:16 -0500

    Fix env variable not inherited from zsh

--- home/.i3/screenshot.sh
+++ home/.i3/screenshot.sh
@@ -1,3 +1,5 @@
-DESTINATION=$DROPBOX/Screenshots/linux/$(date +"%y%m%d-%H%M%S").png
+#!/bin/sh -e
+
+DESTINATION=~/Insync/Screenshots/linux/$(date +"%y%m%d-%H%M%S").png
 # rm -rf is there to remove empty files if you press escape
 flameshot gui --raw > $DESTINATION || rm -f $DESTINATION
